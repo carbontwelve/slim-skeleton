310544693076e0a7667d6fa85b52f23c91ab3dc3
Simon Dann <simon@photogabble.co.uk>
2016-02-17 08:41:15 +0000

    Cherry-picked from branch issue6

--- public/index.php
+++ public/index.php
@@ -1,29 +1,7 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-
-define('APP_BASE', realpath(__DIR__.DIRECTORY_SEPARATOR.'..'));
-
-session_start();
-
-// Instantiate the container
-$container = new \Slim\Container();
-
-// Load settings into the container
-$container->register(new \App\Providers\SettingsProvider());
-$container->register(new \App\Providers\SessionProvider());
-$container->register(new \App\Providers\ViewProvider());
-$container->register(new \App\Providers\LoggerProvider());
-$container->register(new \App\Providers\ControllerProvider());
-
-// Instantiate the app
-$app = new \Slim\App($container);
-
-// Register middleware
-require __DIR__.'/../app/Http/middleware.php';
-
-// Register routes
-require __DIR__.'/../app/Http/routes.php';
+/** @var \Slim\App $app */
+$app = require_once(__DIR__.'/../bootstrap/app.php');
 
 // Run app
 $app->run();
