5e95e63ab53d19eb7510a700d0f213dbfc208ddc
Simon Dann <simon@photogabble.co.uk>
2016-02-17 15:00:28 +0000

    Adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Source and distribution paths
+
+var src = 'resources/assets/';
+var dest = 'public/assets/';
+
+// Lint Task
+gulp.task('lint', function () {
+    return gulp.src(src+'js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile less
+gulp.task('less', function () {
+    return gulp.src(src+'less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest(dest+'css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src(src+'js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dest+'js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch(src+'js/*.js', ['lint', 'scripts']);
+    gulp.watch(src+'less/*.less', ['less']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'less', 'scripts', 'watch']);
