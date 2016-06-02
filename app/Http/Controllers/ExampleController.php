b461d66360918f8977a54085202412781686b5e7
Simon Dann <simon.dann@halesway.co.uk>
2016-06-02 12:43:12 +0100

    Breaking out pages into their own folder

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -9,6 +9,6 @@ class ExampleController extends BaseController
 {
     public function index(ServerRequestInterface $request, ResponseInterface $response, array $args)
     {
-        return $this->view('index', $response);
+        return $this->view('pages\index', $response);
     }
 }
